7ff327e392003d4e000d5f669955195a3e6e44c8
JoseRamirezkqa <jose.ramirezcacua@gmail.com>
2023-02-08 00:03:06 -0500

    8/02/2023-12:03am

--- index.js
+++ index.js
@@ -38,21 +38,31 @@ cero.addEventListener("click", function (e) { input.value = input.value + 0 })
 borrar.addEventListener("click", function (e) { input.value = "" });
 parentesisUno.addEventListener("click", function (e) { input.value = input.value + "(" });
 parentesisDos.addEventListener("click", function (e) { input.value = input.value + ")" });
+raiz.addEventListener("click", function(e){input.value = "√" + input.value } );
+elevado.addEventListener("click", function(e){input.value = input.value + "²"})
+promedio.addEventListener("click", function(e){input.value = input.value + "%"})
 function igualll() {
     result = input.value;
     let enunciado = result.split('');
     sumarr = 0;
     for (let i = 0; i <= enunciado.length; i++) {
-        if (enunciado[i] == "+") {
-            operacion = "suma";
-        } else if (enunciado[i] == "-") {
-            operacion = "resta";
+        if (enunciado[i] == "²"){
+            operacion = "elevado"
+        }else if(enunciado[i] == "%"){
+            operacion = "promedio"
+            break
         }
         else if (enunciado[i] == "*") {
             operacion = "multiplicacion";
-        }
-        else if (enunciado[i] == "/") {
+        }else if (enunciado[i] == "/") {
             operacion = "division";
+        }else if(enunciado[i] == "√"){
+            operacion = "raiz"
+            break;
+        }else if (enunciado[i] == "+") {
+            operacion = "suma";
+        } else if (enunciado[i] == "-") {
+            operacion = "resta";
         }
     }
     switchoop(operacion);
@@ -63,20 +73,26 @@ function switchoop(op) {
         case "suma":
             let numerosSUm = result.split("+")
             numerosSUm.forEach(numeros => {
-                sumarr += Number(numeros);
+                sumarr += parseInt(numeros);
             });
             input.value = sumarr
             break;
         case "resta":
             let numerosRest = result.split("-");
-            numerosRest.forEach(numeros => {
-                if (rest == false) {
-                    rest = Number(numeros)
-                } else {
-                    rest -= Number(numeros);
+            for (let i = 0; i < numerosRest.length; i++){
+                if(numerosRest[i] != ""){
+                    if (rest == false && i == 0) {
+                        rest = parseInt(numerosRest[i])
+                    }else if(rest == false && i != 0){
+                        rest = -parseInt(numerosRest[i])
+                    }else {
+                        rest -= parseInt(numerosRest[i]);
+                    }
+
                 }
-            });
-            input.value = rest
+            }
+            input.value = rest;
+            rest = false;
             break;
             case "multiplicacion":
             let numerosMult = result.split("*")
@@ -88,6 +104,56 @@ function switchoop(op) {
                 }
             })
             input.value = mult
+            mult = false;
             break;
+            case "division":
+            let div;
+            let numerosDiv = result.split("/");
+            for(let i = 0; i < numerosDiv.length; i++){
+                console.log(div)
+                if(i != 1){
+                    div = parseInt(numerosDiv[i]);
+                }else if( i == 1){
+                    if(parseInt(numerosDiv[i]) == 0){
+                        div = "error"
+                    }else if(parseInt(numerosDiv[i]) != 0){
+                        div /= parseFloat(numerosDiv[i])
+                    }
+                }
+            }
+            input.value = div
+            break
+            case "raiz":
+                let raiz;
+                let numerosRaiz = result.split("√");
+                numerosRaiz.forEach(numeros =>{
+                    if(parseInt(numeros) >=0){
+                     raiz = Math.sqrt(parseInt(numeros))   
+                    }else if(parseInt(numeros) < 0){
+                        raiz = "error"
+                    }
+                })
+                input.value = raiz
+                break;
+            case "elevado":
+                let elevado;
+                let numerosElevado = result.split("²");
+                numerosElevado.forEach(numeros =>{
+                    if(numeros != ""){
+                    elevado = Math.pow(numeros,2)
+                    }
+                })
+                input.value = elevado
+                break;
+            case "promedio":
+                let prom;
+                let numerosProm = result.split("%");
+                for(let i = 0; i < numerosProm.length; i++){
+                    if(numerosProm[i] == ""){
+                        prom = numerosProm[i-1]/100
+                        
+                    }
+                }
+                break
     }
 }
