7c5123094de985d3b3587cf1327fa250dd04ff3f
johnmartinez <johnmartinezolarte@gmail.com>
2023-02-09 11:42:18 -0500

    calculadora con historial

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const input = document.querySelector("#input");
+const input2 = document.querySelector("#input2");
+const reslt2 = document.querySelector("#reslt2");
+let result2 = "";
 let result = "";
 let operacion = '';
 let resulSUma = 0;
 let sumarr = 0;
-let rest = false;
-let mult = false;
+let comprobacion = [];
 const cero = document.querySelector("#cero");
 const uno = document.querySelector("#uno");
 const dos = document.querySelector("#dos");
@@ -20,54 +22,54 @@ const restar = document.querySelector("#menos");
 const dividir = document.querySelector("#division");
 const multiplicar = document.querySelector("#multiplicacion");
 const igual = document.querySelector("#igual");
+
 igual.addEventListener("click", function (e) { igualll() })
-uno.addEventListener("click", function (e) { input.value = input.value + 1 });
-dos.addEventListener("click", function (e) { input.value = input.value + 2 });
-tres.addEventListener("click", function (e) { input.value = input.value + 3 })
-cuatro.addEventListener("click", function (e) { input.value = input.value + 4 });
-cinco.addEventListener("click", function (e) { input.value = input.value + 5 });
-seis.addEventListener("click", function (e) { input.value = input.value + 6 });
-siete.addEventListener("click", function (e) { input.value = input.value + 7 });
-ocho.addEventListener("click", function (e) { input.value = input.value + 8 });
-nueve.addEventListener("click", function (e) { input.value = input.value + 9 });
-sumar.addEventListener("click", function (e) { input.value = input.value + "+" });
-restar.addEventListener("click", function (e) { input.value = input.value + "-" });
-dividir.addEventListener("click", function (e) { input.value = input.value + "/" });
-multiplicar.addEventListener("click", function (e) { input.value = input.value + "*" });
-cero.addEventListener("click", function (e) { input.value = input.value + 0 });
-borrar.addEventListener("click", function (e) { input.value = "" });
-parentesisUno.addEventListener("click", function (e) { input.value = input.value + "(" });
-parentesisDos.addEventListener("click", function (e) { input.value = input.value + ")" });
-raiz.addEventListener("click", function (e) { input.value = "√" + input.value });
-elevado.addEventListener("click", function (e) { input.value = input.value + "²" });
+uno.addEventListener("click", function (e) { input.value = input.value + 1; input2.value = input2.value + 1 });
+dos.addEventListener("click", function (e) { input.value = input.value + 2; input2.value = input2.value + 2 });
+tres.addEventListener("click", function (e) { input.value = input.value + 3; input2.value = input2.value + 3 })
+cuatro.addEventListener("click", function (e) { input.value = input.value + 4; input2.value = input2.value + 4 });
+cinco.addEventListener("click", function (e) { input.value = input.value + 5; input2.value = input2.value + 5 });
+seis.addEventListener("click", function (e) { input.value = input.value + 6; input2.value = input2.value + 6 });
+siete.addEventListener("click", function (e) { input.value = input.value + 7; input2.value = input2.value + 7 });
+ocho.addEventListener("click", function (e) { input.value = input.value + 8; input2.value = input2.value + 8 });
+nueve.addEventListener("click", function (e) { input.value = input.value + 9; input2.value = input2.value + 9 });
+sumar.addEventListener("click", function (e) { input.value = input.value + "+"; input2.value = input2.value + "+" });
+restar.addEventListener("click", function (e) { input.value = input.value + "-"; input2.value = input2.value + "-" });
+dividir.addEventListener("click", function (e) { input.value = input.value + "/"; input2.value = input2.value + "/" });
+multiplicar.addEventListener("click", function (e) { input.value = input.value + "*"; input2.value = input2.value + "*" });
+cero.addEventListener("click", function (e) { input.value = input.value + 0; input2.value = input2.value + 0 });
+borrar.addEventListener("click", function (e) { input.value = ""; input2.value = ""; reslt2.value = "" });
+parentesisUno.addEventListener("click", function (e) { input.value = input.value + "("; input2.value = input2.value + "(" });
+parentesisDos.addEventListener("click", function (e) { input.value = input.value + ")"; input2.value = input2.value + ")" });
+raiz.addEventListener("click", function (e) { input.value = "√" + input.value; input2.value = "√" + input2.value });
+elevado.addEventListener("click", function (e) { input.value = input.value + "^"; input2.value = input2.value + "**" });
 punto.addEventListener("click", function (e) { if (input.value != 0) { input.value = input.value + "." } else if (input.value == 0) { input.value = 0 + "." } });
-historial.addEventListener("click",function (e) {$('#displayNone').attr('id','displayBlock'); history();});
+historial.addEventListener("click", function (e) { $('#displayNone').attr('id', 'displayBlock'); history(); });
 let historialArray = [];
 let textHistory = document.getElementById('textHistory')
+let resultadoHistorial
+let saltoDeLinea = "</br>"
+bandera = false;
 function igualll() {
     if (input.value != input.placeholder) {
         result = input.value;
+        result2 = input2.value;
+        historialArray.push(result)
         let enunciado = result.split('');
-        sumarr = 0;
         for (let i = 0; i <= enunciado.length; i++) {
-            if (enunciado[i] == "²") {
-                operacion = "elevado"
-            } else if (enunciado[i] == "%") {
-                operacion = "promedio"
-                break
-            }
-            else if (enunciado[i] == "*") {
-                operacion = "multiplicacion";
-            } else if (enunciado[i] == "/") {
-                operacion = "division";
-            } else if (enunciado[i] == "√") {
+            if (enunciado[i] == "√") {
                 operacion = "raiz"
+                bandera = true;
                 break;
-            } else if (enunciado[i] == "+") {
-                operacion = "suma";
-            } else if (enunciado[i] == "-") {
-                operacion = "resta";
+            } else {
+                resultadoHistorial = eval(result2);
+                input2.value = resultadoHistorial
+                input.value = input2.value;
             }
+        }
+        if (bandera == false) {
+            historialArray.push(resultadoHistorial);
+            resultadoHistorial = "";
         }
         switchoop(operacion);
     }
@@ -75,69 +77,7 @@ function igualll() {
 result = input.value;
 function switchoop(op) {
     switch (op) {
-        case "suma":
-            let numerosSUm = result.split("+")
-            numerosSUm.forEach(numeros => {
-                sumarr += parseFloat(numeros);
-            });
-            input.value = sumarr;
-            historialArray.push(result);
-            historialArray.push(sumarr);
-            break;
-        case "resta":
-            let numerosRest = result.split("-");
-            for (let i = 0; i < numerosRest.length; i++) {
-                if (numerosRest[i] != "") {
-                    if (rest == false && i == 0) {
-                        rest = parseFloat(numerosRest[i])
-                    } else if (rest == false && i != 0) {
-                        rest = -parseFloat(numerosRest[i])
-                    } else {
-                        rest -= parseFloat(numerosRest[i]);
-                    }
-
-                }
-            }
-            input.value = rest;
-            historialArray.push(result);
-            historialArray.push(rest);
-            rest = false;
-            break;
-        case "multiplicacion":
-            let numerosMult = result.split("*")
-            numerosMult.forEach(numeros => {
-                if (mult == false) {
-                    mult = numeros;
-                } else {
-                    mult = mult * numeros
-                }
-            })
-            input.value = mult
-            historialArray.push(result);
-            historialArray.push(mult);
-            mult = false;
-            break;
-        case "division":
-            let div;
-            let numerosDiv = result.split("/");
-            for (let i = 0; i < numerosDiv.length; i++) {
-                console.log(div)
-                if (i != 1) {
-                    div = parseFloat(numerosDiv[i]);
-                } else if (i == 1) {
-                    if (parseFloat(numerosDiv[i]) == 0) {
-                        div = "error"
-                    } else if (parseFloat(numerosDiv[i]) != 0) {
-                        div /= parseFloat(numerosDiv[i])
-                    }
-                }
-            }
-            input.value = div
-            historialArray.push(result);
-            historialArray.push(div);
-            break
         case "raiz":
-            let raiz;
             let numerosRaiz = result.split("√");
             numerosRaiz.forEach(numeros => {
                 if (parseFloat(numeros) >= 0) {
@@ -146,32 +86,22 @@ function switchoop(op) {
                     raiz = "error"
                 }
             })
-            input.value = raiz
-            historialArray.push(result);
             historialArray.push(raiz);
-            break;
-        case "elevado":
-            let elevado;
-            let numerosElevado = result.split("²");
-            numerosElevado.forEach(numeros => {
-                if (numeros != "") {
-                    elevado = Math.pow(numeros, 2)
-                }
-            })
-            input.value = elevado
-            historialArray.push(result);
-            historialArray.push(elevado);
+            input.value = raiz
+            bandera = false;
             break;
 
     }
 }
-function history(){
-    if (historialArray.length != 0 ){
-        for( i =0; i < historialArray.length; i += 2){
-          let text = document.createTextNode(`Operación: ${historialArray[i]}; resultado: ${historialArray[i+1]}`+"\n");
-          textHistory.appendChild(text)
+function history() {
+    if (historialArray.length != 0) {
+        for (i = 0; i < historialArray.length; i += 2) {
+            const histo = document.querySelector(".historial");
+            let html =`Operación: ${historialArray[i]}; resultado: ${historialArray[i + 1]} <br>`;
+            histo.innerHTML += html;
+
         }
-    }else if(historialArray.length == 0){
+    } else if (historialArray.length == 0) {
         let text = document.createTextNode(`No hay operaciones en el historial`);
         textHistory.appendChild(text);
     }
